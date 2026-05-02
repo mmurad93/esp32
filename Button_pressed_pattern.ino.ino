02bd7ff9b5e99e5a7e96b08d4f682e2e170518f7
mmurad93 <mdmuradkhan9367@gmail.com>
2026-05-02 20:05:34 +0600

    3 button, 3 LED, 3 pattern
    This projects work like-
    if button 1 pressed - 3 LEDs will light up in pattern 1
    if button 2 pressed - 3 LEDs will light up in pattern 2
    if button 3 pressed - 3 LEDs will light up in pattern 3

---
+++ Button_pressed_pattern.ino.ino
@@ -0,0 +1,71 @@
+int ledPins[] = {25, 26, 27};
+int btnPins[] = {16, 17, 18};
+bool b[3];
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  Serial.println("Hello, ESP32!");
+
+  for (int i = 0; i < 3; i++) {
+    pinMode(ledPins[i], OUTPUT);
+  }
+
+  for (int i = 0; i < 3; i++) {
+    pinMode(btnPins[i], INPUT_PULLUP);
+  }
+}
+
+void loop() {
+
+  for (int i = 0; i < 3; i++) {
+    b[i] = digitalRead(btnPins[i]);
+  }
+
+  if (b[0] == LOW && b[1] == HIGH && b[2] == HIGH) {
+    seq_1();
+  }
+  else if (b[0] == HIGH && b[1] == LOW && b[2] == HIGH) {
+    seq_2();
+  }
+  else if (b[0] == HIGH && b[1] == HIGH && b[2] == LOW){
+    seq_3();
+  }
+}
+
+void seq_1() {
+  for (int j = 0; j < 3; j++) {
+    for (int i = 0; i < 3; i++) {
+      digitalWrite(ledPins[i], HIGH); delay(500);
+      digitalWrite(ledPins[i], LOW);
+    }
+  }
+}
+
+void seq_2() {
+  for (int i = 0; i < 3; i++) {
+        digitalWrite(ledPins[0], HIGH);
+        digitalWrite(ledPins[2], HIGH); delay(500);
+        digitalWrite(ledPins[0], LOW);
+        digitalWrite(ledPins[2], LOW);
+        digitalWrite(ledPins[1], HIGH); delay(500);
+        digitalWrite(ledPins[1], LOW);
+  }
+}
+
+void seq_3() {
+  for (int i = 0; i < 3; i++) {
+      digitalWrite(ledPins[0], HIGH); delay(500);
+      digitalWrite(ledPins[0], LOW);  delay(500);
+      digitalWrite(ledPins[0], HIGH); delay(250);
+      digitalWrite(ledPins[1], HIGH); delay(500);
+      digitalWrite(ledPins[0], LOW);
+      digitalWrite(ledPins[1], LOW);  delay(500);
+      digitalWrite(ledPins[0], HIGH); delay(250);
+      digitalWrite(ledPins[1], HIGH); delay(250);
+      digitalWrite(ledPins[2], HIGH); delay(500);
+      digitalWrite(ledPins[0], LOW);
+      digitalWrite(ledPins[1], LOW);
+      digitalWrite(ledPins[2], LOW);  delay(500);
+    }
+}
